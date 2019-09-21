41fd55d9a4806ac9feb1c2a4e78921b08a977004
Jorge L <jorge.alfmur@gmail.com>
2019-09-21 09:53:39 -0500

    build schema

--- graphql/schema.js
+++ graphql/schema.js
@@ -4,6 +4,60 @@ const { buildSchema } = require('graphql');
 const schema = buildSchema(`
     type Query {
         message: String
+        search(term: String, page: Int, limit: Int): [Product]
+    }
+
+    type Mutation {
+        signUp(name: String!, email: String!, password: String!): AuthPayload
+        login(email: String!, password: String!): AuthPayload
+        createProduct(name: String!, brand: String!, price: Float!, image: String): Product
+        updateProduct(id: ID!, name: String, brand: String, price: Float): Product
+        uploadImageToProduct(id: ID!, image: String): Product
+        deleteProduct(id: ID!): Product
+        addProductToCart(ProductId: ID, quantity: Int): Cart
+        updateProductInCart(ProductId: ID, quantity: Int): Cart
+        pullOutProductInCart(productId: ID): Cart
+        createOrder(cartId: ID!): Order
+    }
+
+    type User {
+        id: ID
+        name: String
+        email: String
+        products: [Product]
+        orders: [Order]
+    }
+
+    type AuthPayload {
+        token: String
+        user: User
+    }
+
+    type Product {
+        id: ID
+        name: String
+        image: String
+        brand: String
+        price: Float
+        createdBy: User
+    }
+
+    type Cart {
+        id: ID
+        total: Float
+        tax: Float
+        subTotal: Float
+        owner: User
+        products: [Product]
+    }
+
+    type Order {
+        id: ID
+        code: String
+        total: Float
+        tax: Float
+        subTotal: Float
+        products: [Product]
     }
 `)
 
