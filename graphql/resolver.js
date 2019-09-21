eab7582d6436d15779a6d678e413fccca0b35614
Jorge L <jorge.alfmur@gmail.com>
2019-09-21 14:25:05 -0500

    sign up resolver - not work yet

--- graphql/resolver.js
+++ graphql/resolver.js
@@ -1,6 +1,27 @@
+const User = require('../models/user')
+const Product = require('../models/product')
+const Cart = require('../models/cart')
+const Order = require('../models/order')
+const bcrypt = require('bcrypt');
+
+const signUp = async (_, { name, email, password }, ctx) => {
+    console.log(email)
+    const user = await User.create({
+        name,
+        email,
+        password
+    })
+
+    return {
+        token: 'esto deberia ser un token',
+        user
+    }
+}
+
 // Root resolver
 const root = {
-    message: () => 'claro p +cota'
+    message: () => 'claro p +cota',
+    signUp,
 }
 
 module.exports = root
